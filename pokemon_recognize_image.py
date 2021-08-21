cf231e232aa8b394fad55641e39c5c0e2728d0f4
Mkamono <chengyaye31@gmail.com>
2021-08-21 16:10:05 +0900

    update

--- pokemon_recognize_image.py
+++ pokemon_recognize_image.py
@@ -24,7 +24,7 @@ def make_gray_data(filenumber):#一つの画像を読み込んでベクトルに
     return data
 
 def make_data_array(images):#画像のベクトルを行列にする
-    for i in range(0,images+1): #とりあえず10個にしてるよ
+    for i in range(0,images+1):
         filenumber = str(i).zfill(3)   #1→001とかに変換
         if i == 0: #バイアスユニットの生成
             data_list = [make_gray_data("001")]
@@ -51,10 +51,13 @@ def sigmoid(array):
                 answer[row][column] =1.0/(1.0 + math.exp(array[row][column]))
     return answer
 
+
+'''
 def h_theta(x, theta):
     z = np.dot(x,theta)
     h_theta = sigmoid(z)
     return h_theta
+'''
 
 #def CostFunction(x,y,theta,lam):
 
@@ -64,9 +67,31 @@ def h_theta(x, theta):
 
 #def Predict():
 
+def load_data(file_path):
+    img = Image.open(file_path)
+    gray_img = img.convert('L')
+    width, height = gray_img.size
+    gray_img_array = np.empty((height, width), dtype='int')  #make empty array having the size of image data
+    for y in range(height):
+        for x in range(width):
+            gray_img_array[y][x] = gray_img.getpixel((x,y))
+    data = gray_img_array.reshape(-1)  #reshape array into vector
+    data = np.insert(data, [0], 1) #バイアス追加
+    data = data[np.newaxis, :].T
+    return data
+
+num_pokemon = 3 #判別するポケモンの種類の数、アウトプット
 data_list = make_data_array(3) #読み込む画像の枚数 マックス890枚くらい
-theta_1 = np.zeros((160000,1))
-theta_1 += 1   #これはなんとなく
-print("\ndate_list_shape = ", data_list.shape)
+theta_1 = np.zeros((25, 160001))
+theta_2 = np.zeros((26, num_pokemon)) #想定しているのはinput,output含め四層構造
+loaded_data = load_data('/mnt/chromeos/GoogleDrive/MyDrive/python/spyder/script_file/B2programing/pokemon.json-master/images/001.png')
+
+print("\ndata_list_shape = ", data_list.shape)
 print("theta_1_shape = ", theta_1.shape)
-print("h_theta_1 = \n", h_theta(data_list, theta_1)) # \nは正規表現で言う改行だよ
+print("theta_2_shape = ", theta_2.shape)
+print("loaded_data_shape = ", loaded_data.shape)
+print(sigmoid(loaded_data))
+print()
+print(np.dot(theta_1, loaded_data))
+print()
+print()
