b65700e5d70a39dbcba973686e2ea782ec3e2d26
Mkamono <chengyaye31@gmail.com>
2021-09-04 08:27:38 +0900

    update

--- letter_recognization.py
+++ letter_recognization.py
@@ -11,6 +11,7 @@ import os
 import math
 import sys
 import scipy.io
+import random
 
 def make_gray_data(filepath):#一つの画像を読み込んでベクトルにする作業
     img = Image.open(filepath)
@@ -85,60 +86,76 @@ def CostFunction(x,y,theta,lam):
     Cost = 0 
     for m in range(num_data_list):
         y_m = ((y[m])[:,np.newaxis])
-        log = np.log(Predict(x[m],theta[0],theta[1],theta[2])[0])
-        Cost += np.sum((y_m*log)+((1-y_m)*(1-log)))
-    Cost += lam*(np.sum(theta[0]**2) + np.sum(theta[1]**2) + np.sum(theta[2]**2))
+        log = np.log(Predict(x[m],theta[0],theta[1])[1])
+        Cost += np.sum(y_m*log)+np.sum((1-y_m)*(1-log))
+    Cost += lam*(np.sum(theta[0]**2) + np.sum(theta[1]**2))
     return Cost
 
-def Backpropagation(x,y,theta):
+def Backpropagation(x,y,theta,lam):
     num_data_list = x.shape[0]
     DELTA_1 = []
     DELTA_2 = []
-    for m in range(num_data_list):
-        x_m = x[m]
-        y_m = y[m][:, np.newaxis]
+    m = num_data_list
 
-        (a4, a2, a3) = Predict(x_m,theta[0],theta[1],theta[2])
-        a1 = addBias(x_m)
+    for iter in range(10):
+        for M in range(num_data_list):
+            x_m = x[M]
+            y_m = y[M][:, np.newaxis]
+            a1 = addBias(x_m)
+            (a2, a3) = Predict(x_m,theta[0],theta[1])
+            
+            delta_3 = y_m - a3
+            delta_2 = (np.dot(theta[1].T, delta_3))*((a2)*(1-a2))
+            delta_2 = np.delete(delta_2,0,0) #delta[0]を除去
+            if M == 0:
+                DELTA_2 = np.dot(delta_3,a2.T)
+                DELTA_1 = np.dot(delta_2,a1.T)
+            else:
+                DELTA_2 += np.dot(delta_3,a2.T)
+                DELTA_1 += np.dot(delta_2,a1.T)
+        D_1 = calculate_D(theta[0],DELTA_1,lam,m)
+        D_2 = calculate_D(theta[1],DELTA_2,lam,m)
+        Refresh_theta(theta, D_1, D_2)
 
-        delta_3 = y_m - a4
-        delta_2 = (np.dot(theta[1].T, delta_3))*((a2)*(1-a2))
-        delta_2 = np.delete(delta_2,0,0) #delta[0]を除去
-        if m == 0:
-            DELTA_2 = np.dot(delta_3,a2.T)
-            DELTA_1 = np.dot(delta_2,a1.T)
-        else:
-            DELTA_2 += np.dot(delta_3,a2.T)
-            DELTA_1 += np.dot(delta_2,a1.T)
+        print(CostFunction(x, y, theta, lam))
 
-    return(DELTA_1.shape,DELTA_2.shape)
+
+    return(CostFunction(x, y, theta, lam), theta)
  
+def calculate_D(theta,DELTA,lam,m):
+    D = np.zeros((theta.shape))
+    for i in range(theta.shape[0]):
+        if i == 0:
+            D[0] = (1/m) * DELTA[0]
+        else:
+            D[i] = (1/m) * (DELTA[i] + (lam * theta[i]))
+    return D
 
-#def Refresh_theta(J_theta):
 
-def Predict(data_list, theta_1, theta_2, theta_3):
+def Refresh_theta(theta,D1,D2):
+    theta[0] = theta[0] - D1
+    theta[1] = theta[1] - D2
+    return
+
+def Predict(data_list, theta_1, theta_2):
     data_list = addBias(data_list) #バイアス追加
-    a2 = addBias(np.dot(theta_1, data_list))
-    a3 = np.dot(theta_2, a2)
-    z = a3 * theta_3.T
-    h_theta_x = sigmoid(z)
-    return h_theta_x, a2, a3
+    a2 = sigmoid(addBias(np.dot(theta_1, data_list)))
+    a3 = sigmoid(np.dot(theta_2, a2))
+    return a2, a3
 
-def PrintResult(data_list,theta_1,theta_2,theta_3,loaded_data):
+def PrintResult(data_list,theta_1,theta_2,loaded_data):
     print("\ndata_list_shape = ", data_list.shape)
     print("theta_1_shape = ", theta_1.shape)
     print("theta_2_shape = ", theta_2.shape)
-    print("theta_3_shape = ", theta_3.shape)
     print("loaded_data_shape = ", loaded_data.shape)
     print()
     print("predict_result =")
-    print(Predict(loaded_data, theta_1, theta_2, theta_3)[0])
+    print(Predict(loaded_data, theta_1, theta_2)[1])
 
 def make_theta(outputs):
     theta_list = []
     theta_list.append(np.zeros((25, 401)))
     theta_list.append(np.zeros((outputs, 26))) #想定しているのはinput,output含め四層構造
-    theta_list.append(np.zeros((1, outputs)))
     return theta_list
 
 #files = glob2.glob('/mnt/chromeos/GoogleDrive/MyDrive/python/spyder/script_file/B2programing/pokemon.json-master/images/*.png')
@@ -150,9 +167,9 @@ outputs = 10 #判別するポケモンの種類の数、アウトプット
 #data_list = make_data_array(0, files) #読み込む画像の枚数 マックス890枚くらい
 theta_list = make_theta(outputs) #theta 初期化
 
-#PrintResult(data_list,theta_list[0],theta_list[1],theta_list[2],loaded_data)
+#PrintResult(data_list,theta_list[0],theta_list[1],loaded_data)
 
-#print("\nlog(hx) = \n",np.log(Predict(loaded_data,theta_list[0],theta_list[1],theta_list[2])[0]))
+#print("\nlog(hx) = \n",np.log(Predict(loaded_data,theta_list[0],theta_list[1])[0]))
 
 """以下matファイルを使った作業"""
 #参考　https://www.delftstack.com/ja/howto/python/read-mat-files-python/#python-%25E3%2581%25A7-numpy-%25E3%2583%25A2%25E3%2582%25B8%25E3%2583%25A5%25E3%2583%25BC%25E3%2583%25AB%25E3%2582%2592%25E4%25BD%25BF%25E7%2594%25A8%25E3%2581%2597%25E3%2581%25A6mat-%25E3%2583%2595%25E3%2582%25A1%25E3%2582%25A4%25E3%2583%25AB%25E3%2582%2592%25E8%25AA%25AD%25E3%2581%25BF%25E5%258F%2596%25E3%2582%258A%25E3%2581%25BE%25E3%2581%2599
@@ -172,12 +189,12 @@ def y_vector(index): #yの呼び出しのためだけの関数
 print("X-shape = ", X.shape)
 print("y-shape = ", y_label.shape)
 
-print("Predict = \n", Predict(X[0],theta_list[0],theta_list[1],theta_list[2])[0])
+print("Predict = \n", Predict(X[0],theta_list[0],theta_list[1])[1])
 
 J = CostFunction(X, y, theta_list, 0.1)
 print("Cost = ", J)
 
-print(Backpropagation(X, y, theta_list))
+print(Backpropagation(X, y, theta_list, 0.1))
 
-for i in range(3):
+for i in range(2):
     print(theta_list[i].shape)
